57b4002d969ae7e36b13783a1af759edb43301cd
VladyslavPonomaryov <vladyslav.ponomaryov@ab-inbev.com>
2021-09-29 20:23:43 +0300

    add static/media

--- mysite/settings.py
+++ mysite/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'myapp',
+    'storages'
 ]
 
 MIDDLEWARE = [
@@ -141,3 +142,24 @@ else:
         from .settings_local import *
     except ImportError:
         pass
+
+
+# Optional
+AWS_S3_OBJECT_PARAMETERS = {
+    'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+    'CacheControl': 'max-age=94608000',
+}
+# Required
+AWS_STORAGE_BUCKET_NAME = 'python8'
+AWS_S3_REGION_NAME = 'us-east-1'  # e.g. us-east-2
+AWS_ACCESS_KEY_ID = os.environ.get('KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.environ.get('ACCESS_KEY')
+# НЕ ВПИСЫВАЙТЕ САМИ КЛЮЧИ, ТОЛЬКО os.environ.get('SOME_KEY')
+
+# Tell the staticfiles app to use S3Boto3 storage when writing the collected static files (when
+# you run `collectstatic`).
+STATICFILES_LOCATION = 'static'
+STATICFILES_STORAGE = 'custom_storages.StaticStorage'
+
+MEDIAFILES_LOCATION = 'media'
+DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
